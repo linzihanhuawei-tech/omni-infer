63bbc9c7c2ac96d01994bfd5034e0645e43b0880
xuzhou <13290430+xz6546@user.noreply.gitee.com>
2025-10-23 08:55:51 +0000

    update omni/adaptors/vllm/patches/stop_checker_patch.py.
    Signed-off-by: xuzhou <13290430+xz6546@user.noreply.gitee.com>

--- omni/adaptors/vllm/patches/stop_checker_patch.py
+++ omni/adaptors/vllm/patches/stop_checker_patch.py
@@ -1,6 +1,9 @@
 import os
 from vllm.logger import init_logger
 from vllm.v1.request import Request,RequestStatus
+from vllm.v1.core.sched import utils
+
+original_check_stop = utils.check_stop
 
 logger = init_logger(__name__)
 
@@ -63,22 +66,9 @@ def check_stop(request: Request, max_model_len: int) -> bool:
             # prompt + 输出的max_tokens不能超过max_model_len
             if prompt_len + request.max_tokens < max_model_len:
                 request.max_tokens = request.max_tokens + 1
+    
+    return original_check_stop(request,max_model_len)
 
-    if (request.num_tokens >= max_model_len or request.num_output_tokens >= request.max_tokens):
-        request.status = RequestStatus.FINISHED_LENGTH_CAPPED
-        return True
-
-    sampling_params = request.sampling_params
-    last_token_id = request.output_token_ids[-1]
-    if (not sampling_params.ignore_eos and last_token_id == request.eos_token_id):
-        request.status = RequestStatus.FINISHED_STOPPED
-        return True
-
-    if last_token_id in (sampling_params.stop_token_ids or ()):
-        request.status = RequestStatus.FINISHED_STOPPED
-        request.stop_reason = last_token_id
-        return True
-    return False
 
 def patch_enable_max_token_exclude_reasoning():
     from vllm.v1.core.sched import utils
